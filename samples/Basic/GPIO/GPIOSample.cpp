86fc7ade1c4d7ffb74ed7bc8862521f3332c030d
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-04-16 06:02:03 +0000

    Add missing include to samples

--- samples/Basic/GPIO/GPIOSample.cpp
+++ samples/Basic/GPIO/GPIOSample.cpp
@@ -1,27 +1,31 @@
-#include "Sensor.h"
 #include "GPIOSample.h"
+
+#include <stdio.h>
+
+#include "Sensor.h"
 
 using namespace codal;
 
-void onSampleEvent(Event e){
+void onSampleEvent(Event e)
+{
     int value = default_device_instance->io.A3.getAnalogValue();
     default_device_instance->io.D5.setAnalogValue(value);
     printf("A3 = %d\n", value);
 }
 
-
-void GPIOSample_main(){
+void GPIOSample_main()
+{
     codal::STM32PNUCLEO_WB55RG PNUCLEO_WB55RG;
     printf("\n");
     printf("*******************************************\n");
     printf("*           Demonstration de I/O          *\n");
     printf("*******************************************\n");
-    //PNUCLEO_WB55RG.io.A1.setAnalogValue(512);
+    // PNUCLEO_WB55RG.io.A1.setAnalogValue(512);
     PNUCLEO_WB55RG.sleep(1000);
     system_timer_event_every(1000, ID_PIN_P0, SENSOR_UPDATE_NEEDED);
     EventModel::defaultEventBus->listen(ID_PIN_P0, SENSOR_UPDATE_NEEDED, onSampleEvent);
 
-    while(true) {
+    while (true) {
         PNUCLEO_WB55RG.io.led1.setDigitalValue(1);
         PNUCLEO_WB55RG.sleep(1000);
 
--- samples/Peripherals/Serial/SerialSample.cpp
+++ samples/Peripherals/Serial/SerialSample.cpp
@@ -1,16 +1,19 @@
 #include "SerialSample.h"
 
-void SerialSample_main(){
+#include <stdio.h>
+
+void SerialSample_main()
+{
     codal::STM32PNUCLEO_WB55RG PNUCLEO_WB55RG;
-    
+
     printf("\n");
     printf("*******************************************\n");
     printf("* Demonstration de la communication serie *\n");
     printf("*******************************************\n");
-    
+
     PNUCLEO_WB55RG.sleep(1000);
-    
-    while(true) {
+
+    while (true) {
         PNUCLEO_WB55RG.io.led1.setDigitalValue(1);
 
         printf("\nVALUE = %d.%d degC\n", 0, 0);
